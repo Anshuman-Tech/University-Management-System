188883b11ce3f446649cfd3cd6e3324131317817
Anshuman Singh <anshuman20november@gmail.com>
2023-03-09 13:03:22 +0530

    Started with the project

---
+++ src/test/java/com/ums/university/management/system/UniversityManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ums.university.management.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
